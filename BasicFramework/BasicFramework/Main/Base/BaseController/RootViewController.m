76945d8030e4f6bebb10ca88f73fee09d7f3fd13
wangli <2808699200@qq.com>
2016-09-21 17:56:41 +0800

    add MYBlurIntroductionView and implete intruductionView

--- BasicFramework/BasicFramework/Main/Base/BaseController/RootViewController.m
+++ BasicFramework/BasicFramework/Main/Base/BaseController/RootViewController.m
@@ -8,7 +8,8 @@
 
 #import "RootViewController.h"
 #import "RAYNewFunctionGuideVC.h"
-@interface RootViewController ()
+#import "MYBlurIntroductionView.h"
+@interface RootViewController ()<MYIntroductionDelegate>
 
 @end
 
@@ -17,14 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
 }
 - (void)viewDidAppear:(BOOL)animated{
-    [self makeGuideView];
+//    [self makeGuideView];
+    [self makeUserGuide];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+// 用户引导页实现
+- (void)makeUserGuide{
+    MYIntroductionPanel *panel = [[MYIntroductionPanel alloc] initWithFrame:self.view.frame title:@"test1" description:@"description1" image:[UIImage imageNamed:@"introduction"]];
+    
+    MYBlurIntroductionView *introductionView = [[MYBlurIntroductionView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    introductionView.delegate = self;
+    introductionView.BackgroundImageView.image = [UIImage imageNamed:@"introduction"];
+    
+    NSArray *panels = @[panel];
+    
+    [introductionView buildIntroductionWithPanels:panels];
+    
+    [self.view addSubview:introductionView];
+}
+// 聚光灯指示使用
 - (void)makeGuideView{
     RAYNewFunctionGuideVC *vc = [[RAYNewFunctionGuideVC alloc]init];
     vc.titles = @[@"新增: 功能1，",@"新增: 功能2，",@"新增: 功能3，",@"新增: 功能4，",@"新增: 功能5，",];
